0bca6f2d2e08c11d4ba18374e1cd0b05e9b83281
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2006-09-26 12:13:07 +0000

    First revision

---
+++ src/fr/jmmc/mf/gui/ModelFitting.java
@@ -0,0 +1,64 @@
+/*
+ JMMC
+*/
+package jmmc.mf.gui;
+
+import jmmc.mcs.gui.*;
+
+import jmmc.mcs.log.MCSLogger;
+
+import java.util.logging.*;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author mella
+ */
+public class ModelFitting
+{
+    final static String rcsId="$Id: ModelFitting.java,v 1.1 2006-09-26 12:13:07 mella Exp $";
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {        
+        try
+        {
+            // Get a MCSLogger reference  and adjust for convenience
+            // @TODO : move this in MCSLogger
+            Logger logger = MCSLogger.getLogger();
+            logger.setLevel(java.util.logging.Level.ALL);
+            MCSLogger.trace();
+            logger.info("Starting ModelFitting");
+            logger.info("Rev:"+rcsId);
+
+            // Create a specific console handler
+            ConsoleHandler handler = new ConsoleHandler();
+            handler.setLevel(java.util.logging.Level.ALL);
+            logger.addHandler(handler);                      
+            
+            ReportDialog.setDefaultComment( "Please complete above"+
+                    " informations to improve this software.\n"+
+                    rcsId+"\n"+
+                    "---\n");            
+                        
+                        // Uncomment next line to test ReportDialog 
+//            int a=1/0;
+                        
+            MainFrame myFrame = new MainFrame();
+            myFrame.setVisible(true);
+        }
+        catch (Exception e)
+        {
+            
+            new ReportDialog(new javax.swing.JFrame(), true, e).setVisible(true);            
+            System.exit(1);
+        }
+        
+        
+    }
+}
