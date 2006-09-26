964c3db27f66d0ccd53fbec3b42c00ad23e0a683
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2006-09-26 12:44:59 +0000

    Search model.xsd using miscLocateFile

--- src/mfguiGenerateClasses.sh
+++ src/mfguiGenerateClasses.sh
@@ -2,61 +2,19 @@
 #*******************************************************************************
 # JMMC project
 #
-# "@(#) $Id: mfguiGenerateClasses.sh,v 1.1 2006-09-26 12:13:07 mella Exp $"
+# "@(#) $Id: mfguiGenerateClasses.sh,v 1.2 2006-09-26 12:44:59 mella Exp $"
 #
 # History
 # -------
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2006/09/26 12:13:07  mella
+# First revision
+#
 #*******************************************************************************
 
 #/**
 # @file
-# brief description of the shell script, which ends at this dot.
-#
-# @synopsis
-# \<Command Name\> [\e \<param1\> ... \e \<paramN\>] 
-#                     [\e \<option1\> ... \e \<optionN\>] 
-#
-# @param param1 : description of parameter 1, if it exists
-# @param paramN : description of parameter N, if it exists
-#
-# @opt
-# @optname option1 : description of option 1, if it exists
-# @optname optionN : description of option N, if it exists
-# 
-# @details
-# OPTIONAL detailed description of the shell script follows here.
-# 
-# @usedfiles
-# OPTIONAL. If files are used, for each one, name, and usage description.
-# @filename fileName1 :  usage description of fileName1
-# @filename fileName2 :  usage description of fileName2
-#
-# @env
-# OPTIONAL. If needed, environmental variables accessed by the program. For
-# each variable, name, and usage description, as below.
-# @envvar envVar1 :  usage description of envVar1
-# @envvar envVar2 :  usage description of envVar2
-# 
-# @warning OPTIONAL. Warning if any (software requirements, ...)
-#
-# @ex
-# OPTIONAL. Command example if needed
-# \n Brief example description.
-# @code
-# Insert your command example here
-# @endcode
-#
-# @sa OPTIONAL. See also section, in which you can refer other documented
-# entities. Doxygen will create the link automatically. For example, 
-# @sa \<entity to refer\>
-# 
-# @bug OPTIONAL. Known bugs list if it exists.
-# @bug Bug 1 : bug 1 description
-#
-# @todo OPTIONAL. Things to forsee list.
-# @todo Action 1 : action 1 description
-# 
+# brief Generates classes from xsd using castor.
 # 
 # */
 
@@ -71,8 +29,11 @@
 
 # Generate the class path for Java
 CLASSPATH=`mkfMakeJavaClasspath`
+
+MODEL_SCHEMA=$(miscLocateFile mfmdl.xsd)
 # generate model java source from xml schema
-java -classpath $CLASSPATH org.exolab.castor.builder.SourceGenerator -i mfmdl.xsd -f -package jmmc.mf.models  $*
+echo "Generating classes for $MODEL_SCHEMA"
+java -classpath $CLASSPATH org.exolab.castor.builder.SourceGenerator -i ${MODEL_SCHEMA} -f -package jmmc.mf.models  $*
 
 exit 0
 
