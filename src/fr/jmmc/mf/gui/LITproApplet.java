1b1e9845c75039f74e9398ead8dfca138d6bc4b2
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2006-10-31 15:25:00 +0000

    First revision

---
+++ src/fr/jmmc/mf/gui/LITproApplet.java
@@ -0,0 +1,97 @@
+package jmmc.mf.gui;
+
+import jmmc.mcs.gui.*;
+
+import jmmc.mcs.log.MCSLogger;
+
+import java.util.logging.*;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+/*
+ * LITproApplet.java
+ *
+ * Created on 31 octobre 2006, 16:01
+ */
+
+/**
+ *
+ * @author  mella
+ */
+public class LITproApplet extends javax.swing.JApplet {
+
+    final static String rcsId="$Id: LITproApplet.java,v 1.1 2006-10-31 15:25:00 mella Exp $";
+    
+    /** Initializes the applet LITproApplet */
+    public void init() {
+        try {
+            java.awt.EventQueue.invokeAndWait(new Runnable() {
+                public void run() {
+                    initComponents();
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /** This method is called from within the init() method to
+     * initialize the form.
+     */
+    private void initComponents() {
+        startButton = new javax.swing.JButton();
+
+        getContentPane().setLayout(new java.awt.GridBagLayout());
+
+        startButton.setText("Start LITpro GUI");
+        startButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                startButtonActionPerformed(evt);
+            }
+        });
+
+        getContentPane().add(startButton, new java.awt.GridBagConstraints());
+
+    }
+
+    private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {
+
+
+        try
+        {
+            // Get a MCSLogger reference  and adjust for convenience
+            // @TODO : move this in MCSLogger
+            Logger logger = Logger.getLogger("jmmc.mf");
+            logger.setLevel(java.util.logging.Level.ALL);
+            MCSLogger.trace();
+            logger.info("Starting ModelFitting");
+            logger.info("Rev:"+rcsId);           
+
+            // Create a specific console handler
+            ConsoleHandler handler = new ConsoleHandler();
+            handler.setLevel(java.util.logging.Level.ALL);
+            //logger.addHandler(handler);                      
+
+            ReportDialog.setDefaultComment( "Please complete above"+
+                    " informations to improve this software.\n"+
+                    rcsId+"\n"+
+                    "---\n");
+
+            MainFrame myFrame = new MainFrame();
+            myFrame.setVisible(true);
+        }
+        catch (Exception e)
+        {            
+            new ReportDialog(new javax.swing.JFrame(), true, e).setVisible(true);            
+            System.exit(1);
+        }                
+
+    }
+
+
+    // Variables declaration - do not modify
+    private javax.swing.JButton startButton;
+    // End of variables declaration
+
+}
