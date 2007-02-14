d3c7d1e6d28f18448841d6a27b86fd0ba6c8b7ff
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2007-02-14 11:03:49 +0000

    use fr.jmmc instead of jmmc package

---
+++ src/fr/jmmc/mf/gui/SettingsViewerInterface.java
@@ -0,0 +1,19 @@
+/*
+ * SettingsViewerInterface.java
+ *
+ * Created on 6 decembre 2006, 12:07
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+package fr.jmmc.mf.gui;
+
+
+/**
+ *
+ * @author mella
+ */
+public interface SettingsViewerInterface {
+    /** Shows a given object */
+    public void showSettingElement(Object o);
+}
