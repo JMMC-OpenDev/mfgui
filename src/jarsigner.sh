4059603a68094fed42287ed2e6eaefd7e6b984e8
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2007-03-15 09:27:31 +0000

    just for my facilities

---
+++ src/jarsigner.sh
@@ -0,0 +1,2 @@
+jarsigner ../lib/mfgui.jar mykey && cp -f ../lib/mfgui.jar ~/public_html/LITproDev/jar/ && echo ok
+cp -f fr/jmmc/mf/gui/Releases.html ~/public_html/LITproDev/ && echo release copied
