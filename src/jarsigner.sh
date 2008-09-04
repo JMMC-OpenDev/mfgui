6f495dd3f0d4975a92d87f7e6c1657f85aed9e76
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2008-09-04 09:46:15 +0000

    Replaced by jmcDeployJnlp

--- src/jarsigner.sh
+++
@@ -1,4 +0,0 @@
-DESTDIR=/home/mella/jmmc/ysDev/html/
-DESTDIR=~/ysDev/html/
-jarsigner ../lib/mfgui.jar mykey && cp -f ../lib/mfgui.jar $DESTDIR/jar/ && echo ok
-cp -f fr/jmmc/mf/gui/Releases.html $DESTDIR && echo release copied into $DESTDIR
