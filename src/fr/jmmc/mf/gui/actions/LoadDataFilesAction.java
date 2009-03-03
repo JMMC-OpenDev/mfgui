62f2e8d4eeb4d11b39a0ccd219f687e960b53018
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2009-03-03 22:00:06 +0000

    remove toggleFrame call

--- src/fr/jmmc/mf/gui/actions/LoadDataFilesAction.java
+++ src/fr/jmmc/mf/gui/actions/LoadDataFilesAction.java
@@ -12,7 +12,8 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 
-public class LoadDataFilesAction extends RegisteredAction implements TreeSelectionListener, ChangeListener{
+public class LoadDataFilesAction extends RegisteredAction implements TreeSelectionListener, ChangeListener {
+
     public final static String className = "fr.jmmc.mf.gui.actions.LoadDataFileAction";
     public final static String actionName = "loadDataFiles";
     static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
@@ -24,43 +25,35 @@ public class LoadDataFilesAction extends RegisteredAction implements TreeSelecti
 
     public LoadDataFilesAction(MFGui mfgui) {
         super(className, actionName);
-        this.mfgui=mfgui;
+        this.mfgui = mfgui;
     }
 
     public void actionPerformed(ActionEvent e) {
         logger.entering(className, "actionPerformed");
-            try
-            {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setMultiSelectionEnabled(true);
+        try {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setMultiSelectionEnabled(true);
 
-                // Set in previous load directory
-                if (lastDir != null)
-                {
-                    fileChooser.setCurrentDirectory(new java.io.File(lastDir));
-                }
-
-                // Open file chooser
-                int returnVal = fileChooser.showOpenDialog(null);
-
-                if (returnVal == JFileChooser.APPROVE_OPTION)
-                {
-                    java.io.File[] files = fileChooser.getSelectedFiles();
-
-                    for (int i = 0; i < files.length; i++)
-                    {
-                        settingsModel.addFile(files[i]);
-                    }
-
-                    lastDir = files[0].getParent();
-                }
-            }
-            catch (Exception exc)
-            {
-                new FeedbackReport(null, true, exc);
+            // Set in previous load directory
+            if (lastDir != null) {
+                fileChooser.setCurrentDirectory(new java.io.File(lastDir));
             }
 
-        settingsModel.toggleSelectedFrames();
+            // Open file chooser
+            int returnVal = fileChooser.showOpenDialog(null);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                java.io.File[] files = fileChooser.getSelectedFiles();
+
+                for (int i = 0; i < files.length; i++) {
+                    settingsModel.addFile(files[i]);
+                }
+
+                lastDir = files[0].getParent();
+            }
+        } catch (Exception exc) {
+            new FeedbackReport(null, true, exc);
+        }
     }
 
     /** Listen to the settings pane selection changes
@@ -68,10 +61,10 @@ public class LoadDataFilesAction extends RegisteredAction implements TreeSelecti
      * @param e ChangeEvent
      */
     public void stateChanged(ChangeEvent e) {
-        settingsModel=mfgui.getSelectedSettings();
-        if(!settingsModelListener.contains(settingsModel)){
+        settingsModel = mfgui.getSelectedSettings();
+        if (!settingsModelListener.contains(settingsModel)) {
             settingsModel.addTreeSelectionListener(this);
-        }        
+        }
     }
 
     /** Listen to the settings tree selection changes
@@ -79,10 +72,10 @@ public class LoadDataFilesAction extends RegisteredAction implements TreeSelecti
      * @param e TreeSelectionEvent
      */
     public void valueChanged(TreeSelectionEvent e) {
-        if (e.getSource() instanceof SettingsModel){
-            settingsModel = (SettingsModel)e.getSource();            
-        }else{
-            logger.warning("dropped treeSelectionEvent from "+e.getSource());
+        if (e.getSource() instanceof SettingsModel) {
+            settingsModel = (SettingsModel) e.getSource();
+        } else {
+            logger.warning("dropped treeSelectionEvent from " + e.getSource());
         }
     }
 }
