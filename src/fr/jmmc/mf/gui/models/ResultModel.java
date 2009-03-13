f7b4b4a8c48565b01fb4432e1171a10b06ff24f0
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2009-03-13 22:29:57 +0000

    Some duplicated code parts have been moved  to UtilsClass Update according UtilsClass.xsl api change

--- src/fr/jmmc/mf/gui/models/ResultModel.java
+++ src/fr/jmmc/mf/gui/models/ResultModel.java
@@ -2,43 +2,41 @@ package fr.jmmc.mf.gui.models;
 
 import fr.jmmc.mcs.gui.FeedbackReport;
 import fr.jmmc.mf.gui.FrameTreeNode;
-import fr.jmmc.mf.gui.PlotPanel;
 import fr.jmmc.mf.gui.UtilsClass;
 import javax.swing.tree.DefaultMutableTreeNode;
 import fr.jmmc.mf.models.Result;
 import fr.jmmc.mf.models.ResultFile;
+import java.io.File;
 import java.io.StringWriter;
+import java.util.Vector;
 import java.util.logging.Level;
 import javax.swing.JFrame;
 import ptolemy.plot.plotml.PlotMLFrame;
 import ptolemy.plot.plotml.PlotMLParser;
-import ptolemy.plot.Plot;
 
 /**
  * This treeNode brings one Result castor into the JTrees.
  */
 public class ResultModel extends DefaultMutableTreeNode {
+
     public final static String className = "fr.jmmc.mf.gui.models.SettingsModel";
     /** Class logger */
     static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(className);
     private SettingsModel settingsModel;
-
     private String htmlReport = null;
     private String xmlResult = null;
     private Result result;
 
-
     public ResultModel(SettingsModel settingsModel, Result result) {
         this.settingsModel = settingsModel;
-        this.result=result;
+        this.result = result;
 
         try {
             String xslPath = "fr/jmmc/mf/gui/resultToHtml.xsl";
-            java.net.URL url = this.getClass().getClassLoader().getResource(xslPath);
-            StringWriter xmlResultSw= new StringWriter();
+            StringWriter xmlResultSw = new StringWriter();
             result.marshal(xmlResultSw);
-            xmlResult=xmlResultSw.toString();
-            htmlReport = UtilsClass.xsl(xmlResult, url, null);            
+            xmlResult = xmlResultSw.toString();
+            htmlReport = UtilsClass.xsl(xmlResult, xslPath, null);
         } catch (Exception exc) {
             htmlReport = "<html>Error during report generation.</html>";
             new FeedbackReport(null, true, exc);
@@ -46,47 +44,47 @@ public class ResultModel extends DefaultMutableTreeNode {
         /*
         java.net.URL url = this.getClass().getClassLoader().getResource("fr/jmmc/mf/gui/yogaToVoTable.xsl");
         try {
-            java.io.File file = java.io.File.createTempFile("model", "vot");
-            file.deleteOnExit();
-            BufferedWriter out = new BufferedWriter(new FileWriter(file));
-            out.write(UtilsClass.xsl(xmlResult, url, null));
-            out.close();
-            ModelFitting.startFitsViewer(file.getAbsolutePath());
+        java.io.File file = java.io.File.createTempFile("model", "vot");
+        file.deleteOnExit();
+        BufferedWriter out = new BufferedWriter(new FileWriter(file));
+        out.write(UtilsClass.xsl(xmlResult, url, null));
+        out.close();
+        ModelFitting.startFitsViewer(file.getAbsolutePath());
         } catch (Exception exc) {
-            new FeedbackReport(null, true, exc);
+        new FeedbackReport(null, true, exc);
         }
          */
 
         genPlots();
         //genPlots(UtilsClass.getResultFiles(response));
-        this.setUserObject(result);        
+        this.setUserObject(result);
     }
-    
+
     public Result getResult() {
         return (Result) getUserObject();
     }
 
     /*
-     public void genResultReport(SettingsModel settingsModel, Response response) {
-        JFrame resultFrame = new JFrame();
-        JPanel p = new JPanel();
-        p.setLayout(new BorderLayout());
-        resultFrame.getContentPane().add(p);
-        genReport(settingsModel);
-        JScrollPane sp = new JScrollPane(new JEditorPane("text/html", resultPanel.getReport()));
-        p.add(sp);
-//        addPlot(resultFrame, "--New fit result--");
-        genPlots(settingsModel);
-        
-      */
- public String getHtmlReport() {
+    public void genResultReport(SettingsModel settingsModel, Response response) {
+    JFrame resultFrame = new JFrame();
+    JPanel p = new JPanel();
+    p.setLayout(new BorderLayout());
+    resultFrame.getContentPane().add(p);
+    genReport(settingsModel);
+    JScrollPane sp = new JScrollPane(new JEditorPane("text/html", resultPanel.getReport()));
+    p.add(sp);
+    //        addPlot(resultFrame, "--New fit result--");
+    genPlots(settingsModel);
+
+     */
+    public String getHtmlReport() {
         logger.entering("" + this.getClass(), "getHtmlReport");
         return htmlReport;
     }
 
     public void genPlots() {
         logger.entering("" + this.getClass(), "genPlots");
-        String [] plotNames = new String[]{"plotBaselines","plotUVCoverage","plotRadialVIS","plotRadialT3"};
+        String[] plotNames = new String[]{"plotBaselines", "plotUVCoverage", "plotRadialVIS", "plotRadialT3"};
         for (int i = 0; i < plotNames.length; i++) {
             String plotName = plotNames[i];
             ptplot(plotName);
@@ -94,20 +92,30 @@ public class ResultModel extends DefaultMutableTreeNode {
     }
 
     void genPlots(ResultFile[] resultFiles) {
+        // try to locate the corresponding pdf of each png file
         for (int i = 0; i < resultFiles.length; i++) {
             try {
                 ResultFile r = resultFiles[i];
-                ResultFile pdf = null;
+
+                String b64file;
+                File file = null;
+                JFrame f = null;
+                Vector<File> filesToExport = new Vector();
                 if (r.getHref().substring(1, 30).contains("png")) {
                     for (int j = 0; j < resultFiles.length; j++) {
                         ResultFile r2 = resultFiles[j];
                         String filenameWOExt = r2.getName().substring(0, r2.getName().lastIndexOf('.'));
                         if (r.getName().startsWith(filenameWOExt) && r2.getName().endsWith("pdf")) {
-                            pdf = r2;
+                            b64file = r2.getHref();
+                            File pdfFile = UtilsClass.saveBASE64ToFile(b64file, "pdf");
+                            filesToExport.add(pdfFile);
                         }
                     }
-                    JFrame f = PlotPanel.buildFrameOf(r, pdf);
-                    this.add(new FrameTreeNode(f, r.getName()));
+                    b64file = r.getHref();
+                    File pngFile = UtilsClass.saveBASE64ToFile(b64file, "png");
+                    filesToExport.add(pngFile);
+                    f = UtilsClass.buildFrameFor(pngFile);
+                    this.add(new FrameTreeNode(f, r.getName(), filesToExport.toArray(new File[0])));
                 }
             } catch (Exception ex) {
                 logger.log(Level.SEVERE, null, ex);
@@ -120,21 +128,18 @@ public class ResultModel extends DefaultMutableTreeNode {
         logger.entering("" + this.getClass(), "ptplot", plotName);
         String xmlStr = null;
         try {
-            // Contruct xml document to plot
-            java.net.URL url = this.getClass().getClassLoader().getResource("fr/jmmc/mf/gui/yogaToPlotML.xsl");
-            xmlStr = UtilsClass.xsl(xmlResult, url,
+            // Contruct xml document to plot           
+            xmlStr = UtilsClass.xsl(xmlResult, "fr/jmmc/mf/gui/yogaToPlotML.xsl",
                     new String[]{"plotName", plotName});
-            PlotMLParser plotMLParser;
-            // Construct plot and parse xml
-            Plot plot = new Plot();
-            plotMLParser = new PlotMLParser(plot);
-            logger.finest("Trying to plot next document:\n"+xmlStr);
-            plotMLParser.parse(null, xmlStr);
-            // Show plot into frame
-            PlotMLFrame plotMLFrame = new PlotMLFrame("Plotting " + plotName, plot);
-            this.add(new FrameTreeNode(plotMLFrame, plotName));
-        } catch (Exception exc) {            
-            new FeedbackReport(new Exception("Plot generation failed for "+plotName, exc));
+
+            // generate frame and tsv file
+            PlotMLFrame plotMLFrame = UtilsClass.getPlotMLFrame(xmlStr, plotName);
+            File tsv = UtilsClass.getPlotTsv(xmlStr);
+
+            // add on frameTreeNode as child
+            this.add(new FrameTreeNode(plotMLFrame, plotName, tsv));
+        } catch (Exception exc) {
+            new FeedbackReport(new Exception("Plot generation failed for " + plotName, exc));
         }
     }
 }
