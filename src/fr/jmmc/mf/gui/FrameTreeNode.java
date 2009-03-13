d07189860070194ae4ba2202dd96209ec953566c
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2009-03-13 22:09:09 +0000

    The frame can get associated files to be exported( export buttons are handled by FramePanel)

--- src/fr/jmmc/mf/gui/FrameTreeNode.java
+++ src/fr/jmmc/mf/gui/FrameTreeNode.java
@@ -1,5 +1,6 @@
 package fr.jmmc.mf.gui;
 
+import java.io.File;
 import javax.swing.JFrame;
 import javax.swing.tree.DefaultMutableTreeNode;
 
@@ -7,10 +8,38 @@ import javax.swing.tree.DefaultMutableTreeNode;
 /** This class is jst a typed container to present frames into the JTree */
 public class FrameTreeNode extends DefaultMutableTreeNode{
     private String title;
-
+    private File[] filesToExport;
+    private JFrame frame;
+    
     public FrameTreeNode(JFrame frame, String title){
+        init(frame,title,null);
+    }
+
+    public FrameTreeNode(JFrame frame, String title, File fileToExport){
+        init(frame,title, new File[]{fileToExport});
+    }
+
+    public FrameTreeNode(JFrame frame, String title, File[] filesToExport){
+        init(frame,title, filesToExport);
+    }
+
+    private void init(JFrame frame, String title, File[] filesToExport){
         this.title = title;
+        this.frame=frame;
+        if (filesToExport==null){
+            filesToExport=new File[0];
+        }
+        this.filesToExport=filesToExport;
+        frame.setTitle(title);
         setUserObject(frame);
+    }
+
+    public JFrame getFrame(){
+        return frame;
+    }
+
+    public File[] getFilesToExport(){
+        return filesToExport;
     }
 
     @Override
