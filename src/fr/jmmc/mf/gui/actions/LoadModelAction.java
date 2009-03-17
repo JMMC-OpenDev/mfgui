c1cdb2d06cc232ccd8e7f98f1a2b00fe6a934f7a
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2009-03-17 11:34:14 +0000

    Removed 'debug' local filename field

--- src/fr/jmmc/mf/gui/actions/LoadModelAction.java
+++ src/fr/jmmc/mf/gui/actions/LoadModelAction.java
@@ -1,12 +1,13 @@
 package fr.jmmc.mf.gui.actions;
 
 import fr.jmmc.mf.gui.*;
-import fr.jmmc.mcs.gui.FeedbackReport;
 import fr.jmmc.mcs.util.RegisteredAction;
 import fr.jmmc.mf.gui.models.SettingsModel;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.util.logging.Level;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class LoadModelAction extends RegisteredAction {
 
@@ -24,21 +25,25 @@ public class LoadModelAction extends RegisteredAction {
 
     public void actionPerformed(ActionEvent e) {
         logger.entering("" + this.getClass(), "actionPerformed");
-        try {
-            JFileChooser fileChooser = new JFileChooser();
-            // Set in previous load directory
-            if (lastDir != null) {
-                fileChooser.setCurrentDirectory(new File(lastDir));
-            }
-            // Open file chooser
-            int returnVal = fileChooser.showOpenDialog(null);
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
-                lastDir = file.getParent();
+        //try {
+        JFileChooser fileChooser = new JFileChooser();
+        // Set in previous load directory
+        if (lastDir != null) {
+            fileChooser.setCurrentDirectory(new File(lastDir));
+        }
+        // Open file chooser
+        int returnVal = fileChooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            lastDir = file.getParent();
+            try {
                 mfgui.addSettings(new SettingsModel(file));
+            } catch (Exception ex) {
+                String msg = "Can't read '"+file.getName()+"' file. Please load one modelfitting settings file.\n\n(" +
+                        ex.getMessage() + ")";
+                JOptionPane.showMessageDialog(null, msg, "Error ", JOptionPane.ERROR_MESSAGE);
+                logger.log(Level.WARNING, "Can't load settings with selected file", ex);
             }
-        } catch (Exception exc) {
-            new FeedbackReport(null, true, exc);
         }
     }
 }
