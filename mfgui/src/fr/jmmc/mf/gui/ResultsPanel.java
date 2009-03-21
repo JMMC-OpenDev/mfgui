d0b10eb638c3aa9e1d66f414b07a4b9c02784f33
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2009-03-21 06:55:06 +0000

    This commit was manufactured by cvs2svn to create tag 'V1_1_3beta1/mfgui'.

---
+++ mfgui/src/fr/jmmc/mf/gui/ResultsPanel.java
@@ -0,0 +1,34 @@
+package fr.jmmc.mf.gui;
+
+import fr.jmmc.mf.gui.models.SettingsModel;
+import fr.jmmc.mf.models.Results;
+
+public class ResultsPanel extends javax.swing.JPanel {
+    static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
+            "fr.jmmc.mf.gui.ResultsPanel");
+    Results current;
+    SettingsViewerInterface viewer = null;
+    SettingsModel settingsModel = null;
+ 
+    /** Creates new form ResultPanel */
+    public ResultsPanel(SettingsViewerInterface viewer) {
+        this.viewer = viewer;
+        initComponents();
+    }
+
+    public void show(Results r, SettingsModel s) {
+        logger.entering("" + this.getClass(), "show");
+        current = r;
+        settingsModel = s;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setBorder(javax.swing.BorderFactory.createTitledBorder("Results Panel"));
+        setLayout(new java.awt.GridBagLayout());
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
