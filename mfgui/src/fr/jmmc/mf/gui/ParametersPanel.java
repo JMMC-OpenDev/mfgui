70c51d5c5c27d56b8e4401bb2f4b961080adb638
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2010-02-12 13:31:19 +0000

    This commit was manufactured by cvs2svn to create tag 'LITPRO_V1_0_1/mfgui'.

---
+++ mfgui/src/fr/jmmc/mf/gui/ParametersPanel.java
@@ -0,0 +1,174 @@
+package fr.jmmc.mf.gui;
+
+import fr.jmmc.mcs.gui.ShowHelpAction;
+import fr.jmmc.mf.gui.models.ParametersTableModel;
+import fr.jmmc.mf.gui.models.SettingsModel;
+import fr.jmmc.mf.models.Parameter;
+import fr.jmmc.mf.models.Target;
+import fr.jmmc.mf.models.Model;
+import fr.jmmc.mf.models.ParameterLink;
+import fr.jmmc.mf.models.Parameters;
+
+import java.awt.BorderLayout;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Vector;
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class ParametersPanel extends javax.swing.JPanel implements Observer{
+    static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
+            "fr.jmmc.mf.gui.ModelPanel");
+    Parameters current;
+    SettingsModel settingsModel;
+    DefaultListModel parameterListModel;
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton helpButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JTree jTree1;
+    private javax.swing.JTable sharedParametersTable;
+    private javax.swing.JPanel tablePanel;
+    // End of variables declaration//GEN-END:variables
+    private ParametersTableModel sharedTableModel;
+    private Vector<SettingsModel> knownSettingsModels = new Vector();
+
+    /** Creates new form ParametersPanel */
+    public ParametersPanel(SettingsViewerInterface viewer) {
+        initComponents();
+        parameterListModel = new DefaultListModel();
+        sharedTableModel = new ParametersTableModel();
+        sharedParametersTable.setModel(sharedTableModel);
+
+        // build help button
+        helpButton1.setAction(new ShowHelpAction(("ENDtt_SharedParameters")));
+    }
+
+    public void show(SettingsModel s, Parameters p) {
+        settingsModel = s;
+        current = p;
+        parameterListModel.clear();
+        Parameter[] params = s.getSharedParameters();
+        sharedTableModel.setModel(s, params, false);
+        tablePanel.add(sharedParametersTable.getTableHeader(), BorderLayout.NORTH);
+
+        // set one click edition on following table and show all decimals in numerical values        
+        ((DefaultCellEditor)sharedParametersTable.getDefaultEditor(String.class)).setClickCountToStart(1);
+        sharedParametersTable.setDefaultEditor(Double.class, (DefaultCellEditor)sharedParametersTable.getDefaultEditor(String.class));
+        sharedParametersTable.setDefaultRenderer(Double.class, sharedParametersTable.getDefaultRenderer(String.class));
+
+
+        // we want to listen model change events
+        if (!knownSettingsModels.contains(settingsModel)) {
+            settingsModel.addObserver(this);
+            knownSettingsModels.add(settingsModel);
+        }
+
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("Here comes the list of models that get one or more shared parameters");
+        for (int spIndex = 0; spIndex < params.length; spIndex++) {
+            Parameter sharedParameter = params[spIndex];
+            DefaultMutableTreeNode sharedParameterNode = new DefaultMutableTreeNode(sharedParameter);
+            top.add(sharedParameterNode);
+            Target[] targets = s.getRootSettings().getTargets().getTarget();
+            for (int i = 0; i < targets.length; i++) {
+                Target target = targets[i];
+                Model[] models = target.getModel();
+                for (int j = 0; j < models.length; j++) {
+                    Model model = models[j];
+                    ParameterLink[] paramLinks = model.getParameterLink();
+                    for (int k = 0; k < paramLinks.length; k++) {
+                        ParameterLink paramLink = paramLinks[k];
+                        if (paramLink.getParameterRef() == sharedParameter) {
+                            DefaultMutableTreeNode parameterNode = new DefaultMutableTreeNode(""+target+"/"+model+"/"+paramLink.getType());
+                            sharedParameterNode.add(parameterNode);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (params.length==0){
+            top = new DefaultMutableTreeNode("There is no shared parameter yet");
+        }
+
+        jTree1 = new JTree(top);
+        UtilsClass.expandAll(jTree1, true);
+        jScrollPane1.setViewportView(jTree1);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTree1 = new javax.swing.JTree();
+        jSeparator1 = new javax.swing.JSeparator();
+        tablePanel = new javax.swing.JPanel();
+        sharedParametersTable = new javax.swing.JTable();
+        helpButton1 = new javax.swing.JButton();
+
+        setBorder(javax.swing.BorderFactory.createTitledBorder("Parameter list"));
+        setLayout(new java.awt.GridBagLayout());
+
+        jLabel1.setText("Shared parameters:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        add(jLabel1, gridBagConstraints);
+
+        jScrollPane1.setViewportView(jTree1);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weighty = 0.5;
+        add(jScrollPane1, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(4, 0, 4, 0);
+        add(jSeparator1, gridBagConstraints);
+
+        tablePanel.setLayout(new java.awt.BorderLayout());
+
+        sharedParametersTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        tablePanel.add(sharedParametersTable, java.awt.BorderLayout.CENTER);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        add(tablePanel, gridBagConstraints);
+
+        helpButton1.setText("jButton1");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        add(helpButton1, gridBagConstraints);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public void update(Observable o, Object arg) {        
+        if(isShowing()){
+            show(settingsModel, current);
+        }
+    }
+}
---
+++ mfgui/src/fr/jmmc/mf/gui/actions/LoadRemoteDataFilesAction.java
@@ -0,0 +1,112 @@
+package fr.jmmc.mf.gui.actions;
+
+import fr.jmmc.mcs.util.RegisteredAction;
+import fr.jmmc.mf.gui.MFGui;
+import fr.jmmc.mf.gui.models.SettingsModel;
+import fr.jmmc.oidata.RepositoryBrowser;
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+import java.util.Properties;
+import java.util.Vector;
+import java.util.logging.Level;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+
+public class LoadRemoteDataFilesAction extends RegisteredAction implements TreeSelectionListener, ChangeListener {
+
+    public final static String className = "fr.jmmc.mf.gui.actions.LoadRemoteDataFileAction";
+    public final static String actionName = "loadRemoteDataFiles";
+    static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
+            className);
+    MFGui mfgui;
+    SettingsModel settingsModel;
+    Vector<SettingsModel> settingsModelListener = new Vector();
+
+    public LoadRemoteDataFilesAction(MFGui mfgui) {
+        super(className, actionName);
+        this.mfgui = mfgui;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        logger.entering(className, "actionPerformed");
+        RepositoryBrowser browser = new RepositoryBrowser(new JFrame(), true);
+
+        int returnVal = browser.showBrowser();
+
+        if (returnVal == RepositoryBrowser.YES_OPTION) {
+            String browserUrl = browser.getSelectedUrl();
+            try {
+                System.out.println("browserUrl = " + browserUrl);
+
+                // Create a URL for the desired page
+                URL url = new URL(browserUrl);
+
+                // Create a temporary file to receive data
+                File tmpFile = new File(url.getPath());
+                String fileName = tmpFile.getName();
+                String name = fileName.substring(0, fileName.lastIndexOf("."));
+                String ext = fileName.substring(fileName.lastIndexOf("."));
+                fileName = System.getProperties().getProperty("java.io.tmpdir") +
+                        File.separator + name + ext;
+                tmpFile = new File(fileName);
+                tmpFile.deleteOnExit();
+
+                InputStream in = url.openStream();
+                OutputStream out = new FileOutputStream(tmpFile);
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                settingsModel.addFile(tmpFile);
+            } catch (Exception ex) {
+                String msg = "Can't read remote file '" + browserUrl + "'.\n\n(" +
+                        ex.getMessage() + ")";
+                JOptionPane.showMessageDialog(null, msg, "Error ", JOptionPane.ERROR_MESSAGE);
+                logger.log(Level.WARNING, msg, ex);
+            }
+        }
+
+    }
+
+    /** Listen to the settings pane selection changes
+     *
+     * @param e ChangeEvent
+     */
+    public void stateChanged(ChangeEvent e) {
+        settingsModel = mfgui.getSelectedSettings();
+        if (settingsModel == null) {
+            return;
+        }
+        if (!settingsModelListener.contains(settingsModel)) {
+            settingsModel.addTreeSelectionListener(this);
+        }
+    }
+
+    /** Listen to the settings tree selection changes
+     *
+     * @param e TreeSelectionEvent
+     */
+    public void valueChanged(TreeSelectionEvent e) {
+        if (e.getSource() instanceof SettingsModel) {
+            settingsModel = (SettingsModel) e.getSource();
+        } else {
+            logger.warning("dropped treeSelectionEvent from " + e.getSource());
+        }
+    }
+}
