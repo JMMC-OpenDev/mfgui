5b4530f958a7836b56cbf2577c12d932d6a9a619
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2010-05-31 12:57:52 +0000

    Add one personal notebook before the result display

--- src/fr/jmmc/mf/gui/ResultPanel.java
+++ src/fr/jmmc/mf/gui/ResultPanel.java
@@ -10,11 +10,15 @@ public class ResultPanel extends javax.swing.JPanel {
     ResultModel current;
     SettingsViewerInterface viewer = null;
     SettingsModel settingsModel = null;
+    private UserInfoPanel userInfoPanel = null;
  
     /** Creates new form ResultPanel */
     public ResultPanel(SettingsViewerInterface viewer) {
         this.viewer = viewer;
+        userInfoPanel = new UserInfoPanel(viewer);
         initComponents();
+        jPanel2.add(userInfoPanel);
+        
 
         // build help button
         helpButton1.setAction(new ShowHelpAction(("BEG_Results")));
@@ -27,23 +31,42 @@ public class ResultPanel extends javax.swing.JPanel {
         resultEditorPane.setContentType("text/html");
         resultEditorPane.setText(r.getHtmlReport());
         resultEditorPane.setCaretPosition(0);
+        userInfoPanel.show(s.getRootSettings());
     }
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
         helpButton1 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         resultEditorPane = new javax.swing.JEditorPane();
 
         setBorder(javax.swing.BorderFactory.createTitledBorder("Result panel:"));
-        setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.Y_AXIS));
+        setLayout(new java.awt.GridBagLayout());
 
         helpButton1.setText("jButton1");
         helpButton1.setAlignmentX(1.0F);
-        add(helpButton1);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHEAST;
+        add(helpButton1, gridBagConstraints);
+
+        jPanel2.setAlignmentX(1.0F);
+        jPanel2.setMaximumSize(new java.awt.Dimension(100, 150));
+        jPanel2.setMinimumSize(new java.awt.Dimension(100, 150));
+        jPanel2.setPreferredSize(new java.awt.Dimension(100, 170));
+        jPanel2.setLayout(new javax.swing.BoxLayout(jPanel2, javax.swing.BoxLayout.LINE_AXIS));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        add(jPanel2, gridBagConstraints);
 
         jScrollPane1.setAlignmentX(1.0F);
 
@@ -56,12 +79,19 @@ public class ResultPanel extends javax.swing.JPanel {
 
         jScrollPane1.setViewportView(jPanel1);
 
-        add(jScrollPane1);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weighty = 1.0;
+        add(jScrollPane1, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton helpButton1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JEditorPane resultEditorPane;
