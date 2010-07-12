595c2b31a83698a622a627e5ed7347292feb3097
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2010-07-12 14:32:18 +0000

    Add help button

--- src/fr/jmmc/mf/gui/UserInfoPanel.java
+++ src/fr/jmmc/mf/gui/UserInfoPanel.java
@@ -1,5 +1,6 @@
 package fr.jmmc.mf.gui;
 
+import fr.jmmc.mcs.gui.ShowHelpAction;
 import fr.jmmc.mf.models.Settings;
 
 
@@ -19,6 +20,9 @@ public class UserInfoPanel extends javax.swing.JPanel
     {
         settingsViewer = viewer;
         initComponents();
+
+        // Set online help
+        jButton1.setAction(new ShowHelpAction(("BEGtt_PersonalNotebook_Bt")));
     }
 
     public void show(Settings s)
@@ -40,16 +44,24 @@ public class UserInfoPanel extends javax.swing.JPanel
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
+        jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         userInfoTextArea = new javax.swing.JTextArea();
 
         setBorder(javax.swing.BorderFactory.createTitledBorder("Personal notebook:"));
-        setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.LINE_AXIS));
+        setLayout(new java.awt.GridBagLayout());
+
+        jButton1.setText("jButton1");
+        jButton1.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        add(jButton1, gridBagConstraints);
 
         userInfoTextArea.setBackground(new java.awt.Color(255, 255, 153));
         userInfoTextArea.setColumns(20);
-        userInfoTextArea.setFont(new java.awt.Font("Monospaced", 0, 10)); // NOI18N
+        userInfoTextArea.setFont(new java.awt.Font("Monospaced", 0, 10));
         userInfoTextArea.setRows(5);
         userInfoTextArea.addCaretListener(new javax.swing.event.CaretListener() {
             public void caretUpdate(javax.swing.event.CaretEvent evt) {
@@ -58,7 +70,13 @@ public class UserInfoPanel extends javax.swing.JPanel
         });
         jScrollPane1.setViewportView(userInfoTextArea);
 
-        add(jScrollPane1);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        add(jScrollPane1, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
     private void userInfoTextAreaCaretUpdate(javax.swing.event.CaretEvent evt)
@@ -68,6 +86,7 @@ public class UserInfoPanel extends javax.swing.JPanel
     }//GEN-LAST:event_userInfoTextAreaCaretUpdate
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea userInfoTextArea;
     // End of variables declaration//GEN-END:variables
