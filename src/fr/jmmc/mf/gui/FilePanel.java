fc357e9ad894df126f84dc35a896226e50479912
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2011-05-19 07:42:35 +0000

    replace old code (in ModelFitting class) which show tables in topcat using new SampSendFits action

--- src/fr/jmmc/mf/gui/FilePanel.java
+++ src/fr/jmmc/mf/gui/FilePanel.java
@@ -1,11 +1,16 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
 package fr.jmmc.mf.gui;
 
 import fr.jmmc.mcs.gui.MessagePane;
 import fr.jmmc.mcs.gui.ShowHelpAction;
+import fr.jmmc.mf.gui.actions.SampSendFits;
 import fr.jmmc.mf.gui.models.SettingsModel;
 import fr.jmmc.mf.models.File;
 import fr.jmmc.oitools.model.*;
 import fr.nom.tam.fits.FitsException;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.logging.Level;
 import ptolemy.plot.plotml.*;
@@ -19,16 +24,17 @@ import org.apache.commons.math.linear.RealMatrixImpl;
  */
 public class FilePanel extends javax.swing.JPanel {
 
-  final static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
-          FilePanel.class.getName());
-  protected SettingsModel settingsModel;
-  File current = null;
-  OIFitsFile oifitsFile_ = null;
-  static Action saveEmbeddedFileAction;
-  static Action checkEmbeddedFileAction;
-  static Action showEmbeddedFileAction;
-  MyListSelectionListener myListSelectionListener;
-  ListModel hduListModel = new DefaultListModel();
+    final static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
+            FilePanel.class.getName());
+    protected SettingsModel settingsModel;
+    File current = null;
+    OIFitsFile oifitsFile_ = null;
+    static Action saveEmbeddedFileAction;
+    static Action checkEmbeddedFileAction;
+    static Action showEmbeddedFileAction;
+    static SampSendFits sampSendFits;
+    MyListSelectionListener myListSelectionListener;
+    ListModel hduListModel = new DefaultListModel();
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addFileButton1;
     private javax.swing.JButton checkFileButton;
@@ -56,69 +62,70 @@ public class FilePanel extends javax.swing.JPanel {
     private javax.swing.JCheckBox visphiCheckBox;
     // End of variables declaration//GEN-END:variables
 
-  /** Creates new form FilePanel */
-  public FilePanel() {
-    // Prepare action before gui construction
-    saveEmbeddedFileAction = new SaveEmbeddedFileAction();
-    showEmbeddedFileAction = new ShowEmbeddedFileAction();
-    checkEmbeddedFileAction = new CheckEmbeddedFileAction();
-    initComponents();
+    /** Creates new form FilePanel */
+    public FilePanel() {
+        // Prepare action before gui construction
+        saveEmbeddedFileAction = new SaveEmbeddedFileAction();
+        showEmbeddedFileAction = new ShowEmbeddedFileAction();
+        checkEmbeddedFileAction = new CheckEmbeddedFileAction();
+        sampSendFits = new SampSendFits();
+        initComponents();
 
-    myListSelectionListener = new MyListSelectionListener();
-    hduList.addListSelectionListener(myListSelectionListener);
-    //fix default state
-    myListSelectionListener.valueChanged(null);
+        myListSelectionListener = new MyListSelectionListener();
+        hduList.addListSelectionListener(myListSelectionListener);
+        //fix default state
+        myListSelectionListener.valueChanged(null);
 
-    // set help buttons
-    helpButton1.setAction(new ShowHelpAction("_Check_embedded_Bt"));
-    helpButton2.setAction(new ShowHelpAction("_fits_fields_and_Show_selected_Bt"));
-    // next boutons are actually not usefull because they are linked onto the same page
-    //helpButton3.setAction(new ShowHelpAction("_Show_UV_Bt"));
-    //helpButton4.setAction(new ShowHelpAction("_Plot_Vis_Bt"));
-    //helpButton5.setAction(new ShowHelpAction("_Plot_Vis2_Bt"));
-    //helpButton6.setAction(new ShowHelpAction("_Plot_T3_Bt"));
-  }
+        // set help buttons
+        helpButton1.setAction(new ShowHelpAction("_Check_embedded_Bt"));
+        helpButton2.setAction(new ShowHelpAction("_fits_fields_and_Show_selected_Bt"));
+        // next boutons are actually not usefull because they are linked onto the same page
+        //helpButton3.setAction(new ShowHelpAction("_Show_UV_Bt"));
+        //helpButton4.setAction(new ShowHelpAction("_Plot_Vis_Bt"));
+        //helpButton5.setAction(new ShowHelpAction("_Plot_Vis2_Bt"));
+        //helpButton6.setAction(new ShowHelpAction("_Plot_T3_Bt"));
+    }
 
-  /**
-   *
-   *
-   * @param file
-   * @param settingsModel
-   * @throws IOException if an io exception occurs writing the file
-   * @throws FitsException
-   */
-  public void show(File file, SettingsModel settingsModel) {
-    this.settingsModel = settingsModel;
-    // Try to load data file
-    oifitsFile_ = null;
-    current = file;
+    /**
+     *
+     *
+     * @param file
+     * @param settingsModel
+     * @throws IOException if an io exception occurs writing the file
+     * @throws FitsException
+     */
+    public void show(File file, SettingsModel settingsModel) {
+        this.settingsModel = settingsModel;
+        // Try to load data file
+        oifitsFile_ = null;
+        current = file;
 
-    oifitsFile_ = UtilsClass.saveBASE64ToFile(current);
+        oifitsFile_ = UtilsClass.saveBASE64ToFile(current);
 
-    // update file info fields
-    nameTextField.setText(file.getName());
+        // update file info fields
+        nameTextField.setText(file.getName());
 
-    // update lists
-    hduList.setListData(oifitsFile_.getOiTables());
-    hduListModel = hduList.getModel();
-    showUVCoverageButton.setEnabled(false);
-    showVisButton.setEnabled(oifitsFile_.hasOiVis());
-    visampCheckBox.setEnabled(oifitsFile_.hasOiVis());
-    visphiCheckBox.setEnabled(oifitsFile_.hasOiVis());
-    showVis2Button.setEnabled(oifitsFile_.hasOiVis2());
-    showT3Button.setEnabled(oifitsFile_.hasOiT3());
-    t3ampCheckBox.setEnabled(oifitsFile_.hasOiT3());
-    t3phiCheckBox.setEnabled(oifitsFile_.hasOiT3());
-    // update button state
-    hduList.setSelectedIndices(new int[]{});
+        // update lists
+        hduList.setListData(oifitsFile_.getOiTables());
+        hduListModel = hduList.getModel();
+        showUVCoverageButton.setEnabled(false);
+        showVisButton.setEnabled(oifitsFile_.hasOiVis());
+        visampCheckBox.setEnabled(oifitsFile_.hasOiVis());
+        visphiCheckBox.setEnabled(oifitsFile_.hasOiVis());
+        showVis2Button.setEnabled(oifitsFile_.hasOiVis2());
+        showT3Button.setEnabled(oifitsFile_.hasOiT3());
+        t3ampCheckBox.setEnabled(oifitsFile_.hasOiT3());
+        t3phiCheckBox.setEnabled(oifitsFile_.hasOiT3());
+        // update button state
+        hduList.setSelectedIndices(new int[]{});
 
-  }
+    }
 
-  /** This method is called from within the constructor to
-   * initialize the form.
-   * WARNING: Do NOT modify this code. The content of this method is
-   * always regenerated by the Form Editor.
-   */
+    /** This method is called from within the constructor to
+     * initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is
+     * always regenerated by the Form Editor.
+     */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
@@ -751,16 +758,24 @@ public class FilePanel extends javax.swing.JPanel {
             super("showEmbeddedFile");
         }
 
-        public void actionPerformed(java.awt.event.ActionEvent e)
-        {          
-                int[] indices = hduList.getSelectedIndices();
-                for (int i = 0; i < indices.length; i++)
-                {
-                   String       filename = oifitsFile_.getName();
-                    ModelFitting.startFitsViewer(filename + "#" + ((indices[i]) + 1));
-                }                       
+        public void actionPerformed(java.awt.event.ActionEvent e) {
+            int[] indices = hduList.getSelectedIndices();
+            for (int i = 0; i < indices.length; i++) {
+                String filename = "file://"+oifitsFile_.getAbsoluteFilePath()+ "#" + ((indices[i]) + 1);
+                //java.io.File file = new java.io.File( filename + "#" + ((indices[i]) + 1));
+                
+                // TODO find a better way to send this url with a simpler manner
+                // and also find a way to send this file to one listener only...
+                //sampSendFits.setUri(file.toURI().toString());
+                sampSendFits.setUri(filename);
+                // commmand must be All Applications to broadcast!!
+                sampSendFits.actionPerformed(                 
+                        new ActionEvent(e.getSource(), e.getID(), "All Applications"));
+            }
         }
+
     }
+
 
     protected class SaveEmbeddedFileAction extends fr.jmmc.mcs.util.MCSAction
     {
@@ -804,4 +819,4 @@ public class FilePanel extends javax.swing.JPanel {
             }
         }
     }
-}
+    }
