bce10797dbffe3bb7858f95048bc22a804d494c7
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2013-07-11 15:49:50 +0000

    Add specific ModelList widgetification

---
+++ src/fr/jmmc/mf/gui/util/ModelList.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.mf.gui.util;
+
+import fr.jmmc.jmcs.util.StringUtils;
+import fr.jmmc.mf.ModelUtils;
+import fr.jmmc.mf.models.Model;
+import java.awt.event.MouseEvent;
+import javax.swing.JList;
+
+/**
+ * Model list that just have a special renderer and generate tooltip messages.
+ * @author mella
+ */
+public class ModelList extends JList<Model>{
+    
+    /*
+     * Constructor
+     */
+    public ModelList(){
+        this.setCellRenderer(new ModelListRenderer());
+    }
+    
+    @Override
+    public String getToolTipText(final MouseEvent evt) {
+        StringBuffer msg=new StringBuffer(256);
+        msg.append("<html>");
+        int pointedIndex = locationToIndex(evt.getPoint());                
+        if(pointedIndex!=-1){                        
+            Model pointedModel = getModel().getElementAt(pointedIndex);
+            // TODO move this block in ModelUtils common area so we can use it in other widgets
+            msg.append("<h1>");
+            String shortDesc = pointedModel.getShortdesc();
+            if(StringUtils.isEmpty(shortDesc)){
+                msg.append(pointedModel.getType());                
+            }else{
+                msg.append(shortDesc);                
+            }
+            if(ModelUtils.isUserModel(pointedModel)){
+                msg.append("<em>( user model )</em>");                
+            }
+            msg.append("</h1>");
+            
+            if(ModelUtils.isUserModel(pointedModel)){
+                msg.append("Usercode: <pre>").append(pointedModel.getCode()).append("</em>");                
+            }                        
+            msg.append("<pre>").append(pointedModel.getDesc()).append("</pre>");
+        }
+        msg.append("</html>");
+        return msg.toString();
+    }
+            
+}
